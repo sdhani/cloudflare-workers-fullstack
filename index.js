07b49626d96173991b5674421bc8291f3157f2fd
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-04-14 18:58:58 -0400

    added a/b style distribution of variants, cleaned up code, added some documentation

--- index.js
+++ index.js
@@ -2,24 +2,43 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+/**
+ * Fetch Variant URLs 
+ *  @param url
+ *  @returns
+ */
+async function getVariants(url){
+  let getVariants = await fetch(url)
+
+  if(getVariants.ok) {
+    return await getVariants.json()
+  } else {
+    return new Error("Fetch Variant URLs failed")
+  }
+}
+
 
 /**
- * Respond to Fetch request
+ * Respond with variant URL in A/B Testing Style
  * @param {Request} request
+ * @returns {Response} variant
  */
 async function handleRequest(request) {
   const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
-  
-  let urls, response = await fetch(url)
+  let variantUrls = await getVariants(url)
 
-  if(response.ok) {
-    urls = await response.json()
-    console.log(urls.variants[0], urls.variants[1])
-  }
+  /* Get last URL index displayed to user */
+  let currentIndexUrl = request.headers.get('Cookie')
+  let currentUrl = variantUrls.variants[currentIndexUrl]
 
-  let prevUrl = 1
-  let displayThisUrl = urls.variants[prevUrl]
-  let urlResponse = await fetch(displayThisUrl)
+  /* Fetch Variant URL object */
+  let variant = await fetch(currentUrl)
+  let body = variant.body;
 
-  return new Response(urlResponse.body);
+  /* A/B Testing style, Switch between control and test variants */
+  currentIndexUrl = (currentIndexUrl == 0) ? 1 : 0
+
+  return new Response(body, {
+    headers: { 'Set-Cookie': previous = currentIndexUrl },
+  });
 }
