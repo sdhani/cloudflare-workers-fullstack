4d0ace0096161c1620e28871fa9e1feaf25417d4
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-04-14 15:41:23 -0400

    added fetch url request from the api logic

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+  const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
+
+  let response = await fetch(url)
+
+  if(response.ok) {
+    console.log("Response")
+    let payload = await response.json()
+    console.log(payload.variants[0], payload.variants[1])
+  }
+
   return new Response("Response of Handle Request", {
     headers: { 'content-type': 'text/plain' },
   });
