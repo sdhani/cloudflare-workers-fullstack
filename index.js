93a6b458d51afdaadbb9abd0a98c28ff5416eac5
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-04-15 11:20:42 -0400

    added transformed component features

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/* 
+  ./index.js 
+  Author: Shania Dhani
+  Last Modified: April 15th 2020 
+*/
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -17,6 +23,41 @@ async function getVariants(url){
   }
 }
 
+
+/**
+ * Transform Variant oldVariant. Return new Variant 
+ *  @param oldVariant  
+ *  @returns newVariant
+ */
+async function changeVariant(oldVariant) {
+  let newVariant = new HTMLRewriter()
+    .on('h1#title', {
+      element(element) {
+        element.setInnerContent(`The Awesomer Variant`)
+      },
+    })
+    .on('title', {
+      element(element) {
+        element.setInnerContent(`Where Awesome People Thrive.`)
+      }
+    })
+    .on('p#description', {
+      element(element) {
+        element.setInnerContent(`Starting out in Open Source. (Tiny but looking to grow)`)
+      }
+    })
+    .on('a', {
+      element(element) {
+        element.setAttribute("href", "https://hunter-college-ossd-spr-2020.github.io/sdhani-weekly/contributions/");
+        element.setInnerContent(`Open Source Contributions`)
+      }
+    })
+    .transform(oldVariant)
+  
+  return newVariant
+}
+
+
 /**
  * Respond with variant URL in A/B Testing Style
  * @param {Request} request
@@ -29,21 +70,24 @@ async function handleRequest(request) {
   /* Get last URL index displayed to user */
   let currentIndexUrl = request.headers.get('Cookie')
   
-  /* If user hasn't visited the site yet,
-     use A/B Testing style to assign user
-     a variant else assign user persisted variant
+  /* If user hasn't visited the site yet, use A/B Testing style 
+     to assign user a variant, else assign user persisted variant
   */
   if(currentIndexUrl === null){
     currentIndexUrl = Math.floor(Math.random() * 2)
-    console.log("NEW VARIANT", currentIndexUrl)
   } 
 
+  /* Fetch Variant URL object */
   let currentUrl = variantUrls.variants[currentIndexUrl]
-  let variant = await fetch(currentUrl) /* Fetch Variant URL object */
+  let variant = await fetch(currentUrl) 
   let body = variant.body;
 
-  return new Response(body, {
+  /* Create Response Variant */
+  let response = new Response(body, {
     headers: { 'Set-Cookie': previous = currentIndexUrl },
   });
+
+  /* Return transformed Response */
+  return changeVariant(response) 
 }
 
